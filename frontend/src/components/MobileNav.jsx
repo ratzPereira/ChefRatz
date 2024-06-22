72cabcf11d483ff8ffb48dddc486c424b9193e37
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2024-06-22 23:29:25 +0000

    finished navbar for desktop and mobile

--- frontend/src/components/MobileNav.jsx
+++ frontend/src/components/MobileNav.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { HiBars3BottomRight } from "react-icons/hi2";
 import { RiCloseCircleLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 function MobileNav({ menuItems, Logo, onClose, onOpen, hideLeft }) {
   return (
@@ -11,10 +12,38 @@ function MobileNav({ menuItems, Logo, onClose, onOpen, hideLeft }) {
       <button onClick={onOpen} className="border border-primary rounded">
         <HiBars3BottomRight className="h-7 w-7" />
       </button>
-      <div>
-        <button>
+      <div
+        className={`transition-all w-full h-full fixed bg-primary z-50 top-0 ${hideLeft} flex justify-center items-center`}
+      >
+        <button onClick={onClose} className="absolute top-32 right-8">
           <RiCloseCircleLine className="h-7 w-7" />
         </button>
+        <div>
+          <ul className="flex flex-col gap-5">
+            {menuItems?.map((menu, index) => (
+              <li key={index}>
+                <Link
+                  to={menu}
+                  className="font-medium capitalize text-secondary text-2xl"
+                >
+                  {menu}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul className="flex items-center gap-4 font-medium mt-10">
+            <li>
+              <button className="text-secondary px-4 py-2 rounded border">
+                Login
+              </button>
+            </li>
+            <li>
+              <button className="text-secondary px-4 py-2 rounded border">
+                Register
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
