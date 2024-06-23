9f7130faca17a27da0d7ac3a89bd63df9bce93a1
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2024-06-23 11:07:34 +0000

    created category page.

--- frontend/src/pages/category/CategoryPage.jsx
+++ frontend/src/pages/category/CategoryPage.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
+import CategoryWrapper from "./CategoryWrapper.jsx";
 
-function CategoryPage(props) {
-    return (
-        <div></div>
-    );
-}
+const CategoryPage = () => {
+  const { category } = useParams();
+
+  return (
+    <div className="px-6 lg:px-12 py-20">
+      <h1 className="text-center text-3xl py-10 font-semibold text-secondary sm:text-6xl sm:leading-relaxed capitalize">
+        {category}
+      </h1>
+      <CategoryWrapper />
+    </div>
+  );
+};
 
 export default CategoryPage;
--- frontend/src/pages/category/CategoryWrapper.jsx
+++ frontend/src/pages/category/CategoryWrapper.jsx
@@ -27,37 +27,37 @@ function CategoryList() {
     <div className="flex flex-wrap items-center justify-center gap-8">
       <CategoryItem
         name="entrees"
-        href="/categorries/entrees"
+        href="/categories/entrees"
         backgroundColor="#f0f5c4"
         color="#59871f"
       />
       <CategoryItem
         name="breakfast"
-        href="/categorries/breakfast"
+        href="/categories/breakfast"
         backgroundColor="#efedfa"
         color="#3c3a8f"
       />
       <CategoryItem
         name="lunch"
-        href="/categorries/lunch"
+        href="/categories/lunch"
         backgroundColor="#e5f7f3"
         color="#1f8787"
       />
       <CategoryItem
         name="desserts"
-        href="/categorries/desserts"
+        href="/categories/desserts"
         backgroundColor="#e8f5fa"
         color="#397a9e"
       />
       <CategoryItem
         name="sides"
-        href="/categorries/sides"
+        href="/categories/sides"
         backgroundColor="#feefc9"
         color="#d16400"
       />
       <CategoryItem
         name="drinks"
-        href="/categorries/drinks"
+        href="/categories/drinks"
         backgroundColor="#ffeae3"
         color="#f0493e"
       />
