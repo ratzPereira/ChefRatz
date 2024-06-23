c93e32e498bf5e5bc0e604726af9bbce3a8d5ba1
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2024-06-23 09:50:15 +0000

    added search bar and title

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { IoIosSearch } from "react-icons/io";
+
+function Hero(props) {
+  return (
+    <div className="px-5 xl:px-10 md:w-1/2 mb-10">
+      <h1 className="mt-6 mb-10 text-5xl xl:text-6xl text-center font-bold text-[#2A3342] leading-normal xl:leading-relaxed">
+        Our Recepie world!
+      </h1>
+      <form
+        action="/search"
+        className="bg-white p-4 rounded relative flex items-center"
+      >
+        <IoIosSearch className="w-5 h-5 mr-2 text-neutral-300" />
+        <input
+          className="outline-none w-full placeholder:text-[#1b2629]"
+          name="query"
+          type="search"
+          placeholder="Search for a recipe"
+          id="search"
+          required=""
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Hero;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,13 @@
+import Hero from "../../components/Hero.jsx";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="container mx-auto">
+      <div className="flex flex-col justify-center items-center w-full py-20">
+        <Hero />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
