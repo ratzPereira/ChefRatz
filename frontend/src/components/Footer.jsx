c6c39e76022f6ea7de74b05431f022fd9150e1e7
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2024-06-25 01:39:32 +0000

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -128,7 +128,7 @@ const Footer = () => {
           </a>
         </div>
         <p className="mt-8 text-base leading-6 text-center text-gray-400">
-          © 2021 SomeCompany, Inc. All rights reserved.
+          © 2024 Jonny D'Enveloper, Inc. All rights reserved.
         </p>
       </div>
     </div>
